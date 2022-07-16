8238899e1a18fce1667fc5e4f9422e87e619a96e
salima <salimaounoughi@gmail.com>
2022-07-16 15:08:19 +0200

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Salima O. on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
